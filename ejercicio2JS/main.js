e742d55bb522b86ae8b254a61cbfb988fc6197af
ibrahivan <ivan.vazquez.cod@gmail.com>
2023-09-20 09:56:43 +0200

    s

---
+++ ejercicio2JS/main.js
@@ -0,0 +1,27 @@
+// Asignar al cargar la página las funciones correspondientes a cada elemento del menú
+
+
+// Funciones
+// Versión básica: crear una función para cada elemento del menú
+// Versión avanzada: crear una sola función cambiando el parámetro (que será la lista correspoondiente)
+// Ver Alumnos
+
+function verAlumnos() {
+    // Array de 5 alumnos
+    // Mostrar con lista de bootstrap los alumnos de la lista
+}
+
+function verProfesores() {
+    // Array de 4 profesores
+    // Mostrar con lista de bootstrap los profesores de la lista
+}
+
+function verCursos() {
+    // Array de 5 cursos
+    // Mostrar con lista de bootstrap los cursos de la lista
+}
+
+function verPersonal() {
+    // Array de 3 nombres de personal
+    // Mostrar con lista de bootstrap los nombres de la lista
+}
